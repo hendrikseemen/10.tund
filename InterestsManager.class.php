23d2cb21393f768a42524e083d39c31c15babbe8
Hendrik Seemen <hendrik7@greeny.cs.tlu.ee>
2015-11-11 13:32:12 +0200

    tunnitöö

---
+++ InterestsManager.class.php
@@ -0,0 +1,187 @@
+<?php
+class InterestsManager {
+	
+	private $connection;
+	private $user_id;
+	
+	//kui tekitan new, siis käivitatakse see funktsioon
+	
+	function __construct($mysqli, $user_id_from_session){
+		
+		$this->connection = $mysqli;
+		$this->user_id = $user_id_from_session;
+		
+		echo "Huvialade haldus käivitatud, kasutaja=".$this->user_id;
+		
+	}
+	
+	function addInterests($add_interest){
+		
+		$response = new StdClass();
+		
+		
+		$stmt = $this->connection->prepare("SELECT id FROM interests WHERE name=?");
+		$stmt->bind_param("s", $add_interest);
+		$stmt->bind_result($id);
+		$stmt->execute();
+		
+		if($stmt->fetch()){
+			
+			
+			$error = new StdClass();
+			$error->id = 0;
+			$error->message = "huviala <strong>".$add_interest."</strong> on juba olemas!";
+			
+			$response->error = $error;
+			
+			return $response;
+			
+		}
+		
+		$stmt->close();
+		
+		$stmt = $this->connection->prepare("INSERT INTO interests (name) VALUES (?)");
+				
+		
+		$stmt->bind_param("s", $add_interest);
+		if($stmt->execute()) {
+			
+			$success = new StdClass();
+			$success->message = "Huviala edukalt sisestatud!";
+			$response->success = $success;
+			
+			
+		}else {
+			
+			//midagi läks katki
+			$error = new StdClass();
+			$error->id = 1;
+			$error->message = "Midagi läks katki!";
+			
+			$response->error = $error;
+			
+			
+		}
+		
+		
+		$stmt->close();
+		
+		return $response;
+		
+		
+	}
+	
+	function createDropdown(){
+		
+		$html = "";
+		
+		$html .= '<select name="new_dd_selection">';
+		//$html .= '<option>1</option>';
+		//$html .= '<option>2</option>';
+		//$html .= '<option>3</option>';
+		//$html .= '<option selected>3</option>';
+		
+		//$stmt = $this->connection->prepare("SELECT id, name FROM interests");
+		$stmt = $this->connection->prepare("SELECT interests.id, interests.name FROM interests
+		LEFT JOIN user_interests ON interests.id = user_interests.interests_id
+		WHERE user_interests.user_id IS NULL OR user_interests.user_id !=?");
+		
+		$stmt->bind_param("i", $this->user_id);
+		$stmt->bind_result($id, $name);
+		$stmt->execute();
+		
+		while($stmt->fetch()){
+			$html .= '<option value="'.$id.'">'.$name.'</option>';
+			
+		}
+		
+		
+		$html .= '</select>';
+		
+		return $html;
+		
+	}
+	
+	function addUserInterest($new_interest_id){
+		
+		$response = new StdClass();
+		
+		//kas sellel kasutajal on see huviala
+		$stmt = $this->connection->prepare("SELECT id FROM user_interests WHERE user_id=? AND interests_id=?");
+		$stmt->bind_param("ii", $this->user_id, $new_interest_id);
+		$stmt->bind_result($id);
+		$stmt->execute();
+		
+		if($stmt->fetch()){
+			
+			
+			$error = new StdClass();
+			$error->id = 0;
+			$error->message = "huviala on sinul juba olemas!";
+			
+			$response->error = $error;
+			
+			return $response;
+			
+		}
+		
+		$stmt->close();
+		
+		$stmt = $this->connection->prepare("INSERT INTO user_interests (user_id, interests_id) VALUES (?,?)");
+				
+		
+		$stmt->bind_param("ii", $this->user_id, $new_interest_id);
+		if($stmt->execute()) {
+			
+			$success = new StdClass();
+			$success->message = "Huviala edukalt sisestatud!";
+			$response->success = $success;
+			
+			
+		}else {
+			
+			//midagi läks katki
+			$error = new StdClass();
+			$error->id = 1;
+			$error->message = "Midagi läks katki!";
+			
+			$response->error = $error;
+			
+			
+		}
+		
+		
+		$stmt->close();
+		
+		return $response;
+		
+		
+	}
+	
+	function getUserInterests(){
+		
+		$html = '';
+		
+		$stmt = $this->connection->prepare("SELECT interests.name FROM user_interests INNER JOIN interests ON 
+		user_interests.interests_id = interests.id WHERE user_interests.user_id = ?");
+		$stmt->bind_param("i", $this->user_id);
+		$stmt->bind_result($name);
+		$stmt->execute();
+		
+		while($stmt->fetch()){
+			
+			$html .= '<p>'.$name.'<p>';
+			
+			
+		}
+		
+		return $html;
+		
+	}
+		
+		
+		
+	
+	
+	
+} ?>
--- data.php
+++ data.php
@@ -1,5 +1,6 @@
 <?php
 	require_once("functions.php");
+	require_once("InterestsManager.class.php");
 	
 	if(!isset($_SESSION["logged_in_user_id"])){
 		header("Location: login.php");
@@ -16,124 +17,65 @@
 		header("Location: login.php");
 	}
 	
+	$InterestsManager = new InterestsManager($mysqli, $_SESSION["logged_in_user_id"]);
 	
 	
-	// Faili üleslaadimine
-	
-	
-	$target_dir = "profile_pics/";
-	$target_file = $target_dir.$_SESSION["logged_in_user_id"].".jpg";
-	
-	
-	if(isset($_POST["submit"])) {
-	
+	if(isset($_GET["add_interest"])){
+		
+		$add_new_response = $InterestsManager->addInterests($_GET["add_interest"]);
 		
 		
-		$uploadOk = 1;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-		// Check if image file is a actual image or fake image
 		
-			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-			if($check !== false) {
-				echo "File is an image - " . $check["mime"] . ".";
-				$uploadOk = 1;
-			} else {
-				echo "File is not an image.";
-				$uploadOk = 0;
-			}
-		
-		// Check if file already exists
-		if (file_exists($target_file)) {
-			echo "Sorry, file already exists.";
-			$uploadOk = 0;
-		}
-		// Check file size
-		if ($_FILES["fileToUpload"]["size"] > 1024000) {
-			echo "Sorry, your file is too large.";
-			$uploadOk = 0;
-		}
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-			$uploadOk = 0;
-		}
-		// Check if $uploadOk is set to 0 by an error
-		if ($uploadOk == 0) {
-			echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-		} else {
-			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-				echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-				
-				header("Location: data.php");
-				
-			} else {
-				echo "Sorry, there was an error uploading your file.";
-			}
-		}
-	
-	}
-	
-	if(isset($_GET["delete"])){
-		
-		unlink($target_file);
-		
-		header("Location: data.php");
 		
 	}
 	
-	
-	
-	
-	
-	
+	if(isset($_GET["new_dd_selection"])){
+		
+		$add_new_userinterest_response = $InterestsManager->addUserInterest($_GET["new_dd_selection"]);
+		
+		
+		
+		
+	}
 	
 ?>
-
-<?php if(isset($_SESSION["login_success_message"])):?>
-	
-	<p style="color:green;"><?=$_SESSION["login_success_message"];?> </p>
-	
-<?php
-
-unset($_SESSION["login_success_message"]);
-
-endif;?>
 
 <p>
 	Tere, <?=$_SESSION["logged_in_user_id"];?>
 	<a href="?logout=1">Logi välja<a>
 </p>
 
-<h2>Profiilipilt</h2>
-
-<?php if(file_exists($target_file)):?>
-
-<div style="width: 200px; height: 200px; background-image:url(<?=$target_file;?>); background-position: center center; background size: cover;"></div>
-
-<a href="?delete=1">Delete profile pic</a>
-
-<?php else: ?>
-
-
-<form action="data.php" method="post" enctype="multipart/form-data">
-    Lae üles pilt (1MB, png, jpg, gif):
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+ <?php if(isset($add_new_response->error)):?>
+  
+	<p style="color:red;"><?=$add_new_response->error->message;?> </p>
+	
+  <?php elseif(isset($add_new_response->success)):?>
+  
+	<p style="color:green;"><?=$add_new_response->success->message;?> </p>
+  
+<?php endif; ?>
+<form >
+    <input name="add_interest">
+    <input type="submit" value="Lisa">
 </form>
 
+<?php if(isset($add_new_userinterest_response->error)):?>
+  
+	<p style="color:red;"><?=$add_new_userinterest_response->error->message;?> </p>
+	
+  <?php elseif(isset($add_new_userinterest_response->success)):?>
+  
+	<p style="color:green;"><?=$add_new_userinterest_response->success->message;?> </p>
+  
 <?php endif; ?>
 
-<?php
+<h2>Minu huvialad</h2>
 
-	$file_array= scandir($target_dir);
-	var_dump($file_array);
+<form >
+    <?=$InterestsManager->createDropdown();?>
+    <input type="submit" value="Lisa">
+</form>
+
+<br><br>
 	
-	for($i = 0; $i < count ($file_array); $i++){
-		
-		echo "<a href='".$target_dir.$file_array[$i]."'>".$file_array[$i]."</a><br>";
-		
-	}
-
-?>
+	<?=$InterestsManager->getUserInterests();?>
